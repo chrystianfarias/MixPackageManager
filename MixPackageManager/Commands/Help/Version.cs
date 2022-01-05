aab2be708c1ec52246b6050fdedbfb34af03c710
Chrystian Farias <chrystian.farias@outlook.com>
2022-01-05 19:50:54 -0300

    version check

---
+++ MixPackageManager/Commands/Help/Version.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MixMods.MixPackageManager
+{
+    public class Version
+    {
+        [Command("v")]
+        public static void Print(Arguments arguments)
+        {
+            Console.WriteLine(Application.ProductVersion);
+        }
+    }
+}
